7995136f497124966b82c9d4a869619a3a13307e
wefdsfa <1317426386@qq.com>
2022-05-19 03:19:11 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("eb12e9c3-ec9d-4718-82bf-9bfc6be360d0");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
